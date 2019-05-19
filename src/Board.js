4c4356f17e143de9d654a6f1e6d1ac96e962448c
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-18 23:14:42 -0700

    cleaned up some code

--- src/Board.js
+++ src/Board.js
@@ -2,33 +2,6 @@ import React, { Component } from "react";
 import Cell from "./Cell";
 import './Board.css';
 
-
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
   static defaultProps = {
     nrows: 5,
@@ -41,10 +14,7 @@ class Board extends Component {
       hasWon: false,
       board: this.createBoard()
     }
-    // TODO: set initial state
   }
-
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
     let board = [];
@@ -81,18 +51,10 @@ class Board extends Component {
     flipCell(y - 1, x);
     flipCell(y + 1, x);
 
-
-    // TODO: flip this cell and the cells around it
-
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
     let hasWon = board.every(row => row.every(cell => !cell));
 
     this.setState({ board, hasWon });
   }
-
-
-  /** Render game board or winning message. */
 
   render() {
     if (this.state.hasWon) {
@@ -105,13 +67,7 @@ class Board extends Component {
         </div>
       )
     }
-    // if the game is won, just show a winning msg & render nothing else
 
-    // TODO
-
-    // make table board
-
-    // TODO
     let tableBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
